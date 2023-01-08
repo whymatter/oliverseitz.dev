00408d47cf879f5915a353a2e3ecae4535c04cba
Oliver <seitz-oliver@gmx.de>
2023-01-08 18:13:51 +0100

    Add Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import '/public/reset.css'
 import '/public/style.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
